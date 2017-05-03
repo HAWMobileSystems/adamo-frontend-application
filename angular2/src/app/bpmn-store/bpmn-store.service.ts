3e19f9bbd85619b9a042d23692c48c047955e592
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@haw-landshut.de>
2017-05-03 09:47:23 +0200

    rename angular folder to angular2 + palette
    again submodule problems.. 
    palette is working as expected and expands

---
+++ angular2/src/app/bpmn-store/bpmn-store.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import { Observable } from "rxjs";
+
+export class Link {
+    constructor(public readonly href: string, public readonly text?: string, public readonly rel?: string, ) {
+        this.text = text || href;
+        this.rel = rel || "none";
+    }
+
+}
+
+@Injectable()
+export class BPMNStore {
+
+    constructor(private http: Http) {
+    }
+
+    listDiagrams(): Observable<Link[]> {
+        console.log('listDiagrams');
+
+        // This could be async and coming from a server:
+        return Observable.of([
+            new Link("/diagrams/initial.bpmn"),
+            new Link("/diagrams/pizza-collaboration.bpmn")
+        ]).delay(2000);
+    }
+
+    paletteEntries(): Observable<any> {
+        // This could be async and coming from a server:
+        return Observable.of({
+            'two-column': {
+                group: 'row',
+                className: ['fa-th-large', 'fa'],
+                title: 'Multi Row',
+                action: {
+                    click: () => console.log('two-column')
+                }
+            },
+            'extra': {
+                group: 'storage',
+                className: ['fa-coffee', 'fa'],
+                title: 'EXTRA',
+                action: {
+                    click: () => console.log('EXTRA')
+                }
+            }
+        }).delay(1000);
+    }
+}
---
+++ angular2/src/app/modeler/modeler.component.ts
@@ -0,0 +1,149 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from "@angular/http";
+
+import { PaletteProvider } from './palette';
+import { CustomPropertiesProvider } from './props-provider';
+import { BPMNStore, Link } from "../bpmn-store/bpmn-store.service";
+
+const modeler = require("bpmn-js/lib/Modeler.js");
+const propertiesPanelModule = require('bpmn-js-properties-panel');
+const propertiesProviderModule = require('bpmn-js-properties-panel/lib/provider/bpmn');
+
+import { CustomModdle } from './custom-moddle';
+import { Observable, Subject } from "rxjs";
+import { ChangeDetectorRef } from '@angular/core';
+
+import * as $ from 'jquery';
+const customPaletteModule = {
+    paletteProvider: ['type', PaletteProvider]
+};
+const customPropertiesProviderModule = {
+    __init__: ['propertiesProvider'],
+    propertiesProvider: ['type', CustomPropertiesProvider]
+};
+
+const containerRef = '#js-canvas';
+const propsPanelRef = '#js-properties-panel';
+
+@Component({
+    templateUrl: './modeler.component.html',
+    styleUrls: ['./modeler.component.css'],
+    providers: [BPMNStore]
+})
+export class ModelerComponent implements OnInit {
+    modeler: any;
+
+    url: string;
+    _urls: Link[];
+    extraPaletteEntries: any;
+    commandQueue: Subject<any>;
+
+    constructor(private http: Http, private store: BPMNStore, private ref: ChangeDetectorRef) {
+    }
+
+    get urls(): Link[] {
+        return this._urls;
+    }
+
+    set urls(u: Link[]) {
+        console.log("urls: ", u);
+        this._urls = u;
+        this.url = u[0].href;
+    }
+
+
+    ngOnInit() {
+        this.commandQueue = new Subject();
+        this.commandQueue
+            .subscribe(cmd => console.log('Received command: ', cmd));
+        this.commandQueue
+            .filter(cmd => 'extra' == cmd.action)
+            .subscribe(cmd => console.log('Received SUPER SPECIAL EXTRA command: ', cmd));
+
+        this.commandQueue
+            .filter(cmd => 'two-column' == cmd.action)
+            .do(cmd => {
+                console.log('two columns', cmd)
+                let palette = $('.djs-palette');
+                palette.hasClass('two-column')
+                    ? this.shrinkToOneColumn(palette) 
+                    : this.expandToTwoColumns(palette)
+                this.ref.detectChanges
+            })
+            .subscribe(cmd => console.log('Received SUPER SPECIAL two-column command: ', cmd));
+        // .do(cmd =>  console.log('two-column', cmd))
+
+        this.commandQueue
+            .filter(cmd => 'save' == cmd.action)
+            .do(cmd => console.log('Received SUPER SPECIAL SAVE command: ', cmd))
+            .subscribe(() => this.modeler.saveXML((err: any, xml: any) => console.log('xml!?!', err, xml)))
+            ;
+
+
+        this.store.listDiagrams()
+            .do(links => this.urls = links)
+            .do(() => console.log('Got links: ', this.urls))
+            .flatMap(() => this.store.paletteEntries())
+            .do(entries => this.extraPaletteEntries = entries)
+            .do(() => console.log('Got entries: ', this.extraPaletteEntries))
+            .subscribe(() => this.createModeler());
+
+    }
+
+    shrinkToOneColumn = (palette: JQuery) => {
+        palette.removeClass('two-column');
+        palette.find('.fa-square').removeClass('fa-square').addClass('fa-th-large')
+    }
+
+    expandToTwoColumns(palette: JQuery) {
+        palette.addClass('two-column')
+        palette.find('.fa-th-large').removeClass('fa-th-large').addClass('fa-square')
+    }
+
+    createModeler() {
+        console.log('Creating modeler, injecting extraPaletteEntries: ', this.extraPaletteEntries);
+        this.modeler = new modeler({
+            container: containerRef,
+            propertiesPanel: {
+                parent: propsPanelRef
+            },
+            additionalModules: [
+                { 'extraPaletteEntries': ['type', () => this.extraPaletteEntries] },
+                { 'commandQueue': ['type', () => this.commandQueue] },
+                propertiesPanelModule,
+                propertiesProviderModule,
+                customPropertiesProviderModule,
+                customPaletteModule,
+            ],
+            moddleExtensions: {
+                ne: CustomModdle
+            },
+        });
+
+        // Start with an empty diagram:
+        this.url = this.urls[0].href;
+        this.loadBPMN();
+    }
+
+    loadBPMN() {
+        console.log('load', this.url, this.store);
+        var canvas = this.modeler.get('canvas');
+        this.http.get(this.url)
+            .map(response => response.text())
+            .map(data => this.modeler.importXML(data, this.handleError))
+            .subscribe(x => x ? this.handleError(x) : this.postLoad())
+            ;
+    }
+
+    postLoad() {
+        var canvas = this.modeler.get('canvas');
+        canvas.zoom('fit-viewport');
+    }
+
+    handleError(err: any) {
+        if (err)
+            console.log('error rendering', err);
+    }
+
+}
+
