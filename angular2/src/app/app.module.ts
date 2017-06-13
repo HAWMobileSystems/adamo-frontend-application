be040b9affe2213c7d40519ac96f368364fdb6e8
Daniel Hilpoltsteiner <dh@campudus.com>
2017-06-13 15:00:57 +0200

    add ng2-bs3-modal
    modals first draft working

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {HttpModule} from "@angular/http";
-import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
-import {AppRoutingModule} from "./app-routing.module";
-import {FrontPageComponent} from "./front-page/front-page.component";
-import {ViewerComponent} from "./viewer/viewer.component";
-import {ModelerComponent} from "./modeler/modeler.component";
+import { AppRoutingModule } from './app-routing.module';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { ModelerComponent } from './modeler/modeler.component';
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 @NgModule({
     imports: [
@@ -15,12 +16,13 @@ import {ModelerComponent} from "./modeler/modeler.component";
         HttpModule,
         AppRoutingModule,
         FormsModule,
+        Ng2Bs3ModalModule
     ],
     declarations: [
         AppComponent,
         FrontPageComponent,
         ViewerComponent,
-        ModelerComponent,
+        ModelerComponent
     ],
     bootstrap: [ AppComponent ]
 })
