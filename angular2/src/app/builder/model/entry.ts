a1b0d4fd57aab2c496cf1202a8f3358101e8ba38
Daniel Hilpoltsteiner <dh@campudus.com>
2017-08-02 08:24:01 +0200

    add some logic to handle multiple files (initial)

---
+++ angular2/src/app/builder/model/entry.ts
@@ -0,0 +1,17 @@
+import * as uuid from 'node-uuid';
+
+export class Entry {
+  public selected: boolean;
+  private title: string;
+  public uid : any;
+
+  public setTitle(title: string) {
+    this.title = title.trim();
+  }
+
+  constructor(title: string) {
+    this.uid = uuid.v4();
+    this.selected = false;
+    this.title = title.trim();
+  }
+}
