46035ebde07213f005a6f95fd68b6bacc65bf4c0
Masood <M.Ahmed2@gmx.de>
2018-03-22 15:28:27 +0100

    Import Testmodul into Testmodul to remove error.

--- angular2/src/app/app-routing.module.ts
+++ angular2/src/app/app-routing.module.ts
@@ -4,14 +4,19 @@ import { ViewerComponent } from './viewer/viewer.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { ModelerComponent } from './modeler/modeler.component';
 import { AuthGuard } from './guards/auth.guard';
+import { TestComponent} from './test/test.component';
 
 const routes: Routes = [
     { path: 'front-page', component: FrontPageComponent },
     { path: 'viewer', component: ViewerComponent, canActivate: [AuthGuard] },
     { path: 'modeler', component: ModelerComponent, canActivate: [AuthGuard] },
 
+    { path: 'test', component: TestComponent},
     { path: '',   redirectTo: '/front-page', pathMatch: 'full' },
     { path: '**', redirectTo: '/front-page', pathMatch: 'full' },
+
+    { path: '',   redirectTo: '/test', pathMatch: 'full' },
+    { path: '**', redirectTo: '/test', pathMatch: 'full' },
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
