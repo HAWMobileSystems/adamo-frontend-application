bcf1329d6320f64d10f78a92fa81b5db8b79175e
Lackmann1994 <lackmann1994@gmail.com>
2018-04-11 14:58:16 +0200

    added Role and Model Administration

--- angular2/src/app/components/ModelComponent/model.component.ts
+++ angular2/src/app/components/ModelComponent/model.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertService} from '../../services/alert.service';
+import {ApiService} from "../../services/api.service";
 
+
+
+
+
+const mqtt = require('mqtt');
 
 @Component({
     selector: 'model-management',
@@ -7,11 +14,102 @@ import { Component } from '@angular/core';
 })
 
 export class ModelComponent {
+    private selected: any;
+    private newModel: any;
+    private models: any;
+    private mqtt: any;
 
-    constructor() {
+
+    constructor(private apiService: ApiService, private alertService: AlertService) {
     }
 
     public ngOnInit() {
+        this.newModel = {
+            mid: '',
+            modelname: '',
+            modelxml: '',
+            version: '',
+            lastchange: ''
+        };
+
+        this.getAllModels();
+
+
+        this.mqtt = mqtt.connect('mqtt://localhost:4711');
+        this.mqtt.subscribe('MODEL');
+        const i = this;
+        this.mqtt.on('message', function (topic: any, message: any) {
+            console.log('Test from remote:' + message.toString());
+            i.getAllModels();
+        });
     }
 
+    public getAllModels() {
+        this.models = [];
+
+        this.apiService.getAllModels()
+            .subscribe(response => {
+                    if (response.success) {
+                        this.models = response.data;
+                        this.selected = null;
+                    }
+                    else {
+                        this.alertService.error(response.error)
+                    }
+                },
+                error => {
+                    console.log(error);
+                });
+    }
+
+
+
+    public modelUpdate() {
+        this.apiService.modelUpdate(this.selected.mid, this.selected.modelname, this.selected.lastchange, this.selected.modelxml, this.selected.version)
+            .subscribe(response => {
+                    if (response.success) {
+                        this.mqtt.publish('MODEL');
+                    }
+                    else {
+                        this.alertService.error(response.error)
+                    }
+                },
+                error => {
+                    this.alertService.error(error.statusText);
+                    console.log(error);
+                });
+    }
+
+    public modelCreate() {
+        this.apiService.modelCreate(this.selected.mid, this.selected.modelname, this.selected.lastchange, this.selected.modelxml, this.selected.version)
+            .subscribe(response => {
+                    if (response.success) {
+                        this.mqtt.publish('MODEL');
+                    }
+                    else {
+                        this.alertService.error(response.error)
+                    }
+                },
+                error => {
+                    this.alertService.error(error.statusText);
+                    console.log(error);
+                });
+    }
+
+    public modelDelete() {
+        this.apiService.modelDelete(this.selected.mid)
+            .subscribe(response => {
+                    console.log(response);
+                    if (response.success) {
+                        this.mqtt.publish('MODEL');
+                    }
+                    else {
+                        this.alertService.error(response.error)
+                    }
+                },
+                error => {
+                    this.alertService.error(error.statusText);
+                    console.log(error);
+                });
+    }
 }
