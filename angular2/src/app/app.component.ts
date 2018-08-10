4aea92ea8ea7f39c084fa9de9693535382e1e623
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@haw-landshut.de>
2018-08-10 17:23:56 +0200

    update app component to use less

--- angular2/src/app/app.component.ts
+++ angular2/src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent  {
 }
