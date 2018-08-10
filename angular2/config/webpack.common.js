7bd0900b26f82a943e4c07799987aa87abc0b2d0
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@haw-landshut.de>
2018-08-10 17:23:28 +0200

    rebuild rules for webpack2 while removing extracttextplugin

--- angular2/config/webpack.common.js
+++ angular2/config/webpack.common.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+//var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
 
 // function isExternal(module) {
@@ -35,50 +35,79 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
 
     module: {
-        loaders: [
-            // {
+        rules: [ // {
             //     test: /\.js$/,
             //     loaders: ['awesome-typescript-loader', 'angular2-template-loader']
             // },
             {
                 test: /\.ts$/,
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
-            },
-            {
+                use: [{
+                    loader: 'awesome-typescript-loader'
+                }, {
+                    loader: 'angular2-template-loader'
+                }]
+            }, {
                 test: /\.html$/,
-                loader: 'html'
-            },
-            {
+                use: [{
+                    loader: 'html-loader'
+                }]
+            }, {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?\d+)?$/,
-                loader: 'file?name=graphics/[name].[ext]'
-            },
-            {
+                use: [{
+                    loader: 'file-loader',
+
+                    options: {
+                        name: 'graphics/[name].[ext]'
+                    }
+                }]
+            }, {
                 test: /\.bpmn$/,
-                loader: 'file?name=diagrams/[name].[ext]'
+                use: [{
+                    loader: 'file-loader',
+
+                    options: {
+                        name: 'diagrams/[name].[ext]'
+                    }
+                }]
             },
             {
                 test: /\.less/,
-                loader: "less"
+                exclude: helpers.root('src', 'app'),
+                use: ['style-loader', 'less-loader']
+                // use: [{
+                //     loader: 'less-loader'
+                // }]
             },
             {
                 test: /\.css$/,
                 exclude: helpers.root('src', 'app'),
-                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+                use: ['to-string-loader', 'css-loader']
+                // use: ExtractTextPlugin.extract({
+                //     fallback: 'style',
+                //     use: 'css?sourceMap'
+                // })
             },
             {
                 test: /\.css$/,
                 exclude: helpers.root('public'),
                 include: helpers.root('src', 'app'),
                 // https://github.com/webpack/style-loader/issues/123:
-                loaders:[ExtractTextPlugin.extract('style', 'css-loader'), 'to-string', 'css', 'less-loader']
-            },
-            {
-                test: /\.json$/,
-                loader: 'json'
+                use: ['style-loader', 'to-string-loader', 'css-loader']
+                //use: ['style-loader', 'css-loader', 'less-loader']
+                // use: 'ignore-loader' [ExtractTextPlugin.extract({
+                //     fallback: 'style',
+                //     use: 'css-loader'
+                // }), {
+                //     loader: 'to-string-loader'
+                // }, {
+                //     loader: 'css-loader'
+                // }, {
+                //     loader: 'less-loader'
+                // }]
             }
         ]
     },
--- angular2/config/webpack.dev.js
+++ angular2/config/webpack.dev.js
@@ -1,24 +1,40 @@
 var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+//var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 var webpack = require('webpack');
 
 module.exports = webpackMerge(commonConfig, {
     devtool: 'source-map',
- module: {
-    loaders: [
-      {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      }, 
-      {
-        test: /\.less$/,
-        exclude: /node_modules/,
-        loader: 'raw-loader!less-loader'
-}
-    ]
-  },
+    module: {
+        rules: [{
+                test: /\.scss$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                // use: ['style-loader', 'less-loader']
+                use: ['style-loader', 'css-loader', 'less-loader']
+                // use: ['style-loader', 'css-to-string-loader', 'css-loader', 'less-loader']
+                // use: ['less-loader']
+
+                //use: ['style-loader', 'raw-loader', 'less-loader']
+
+                //     {
+                //     loader: 'raw-loader'
+                // }, {
+                //     loader: 'less-loader'
+                // }]
+            }
+        ]
+    },
     output: {
         path: helpers.root('dist'),
         publicPath: 'http://localhost:8080/',
@@ -27,7 +43,7 @@ module.exports = webpackMerge(commonConfig, {
     },
 
     plugins: [
-        new ExtractTextPlugin('[name].css'),
+        //  new ExtractTextPlugin('[name].css'),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
