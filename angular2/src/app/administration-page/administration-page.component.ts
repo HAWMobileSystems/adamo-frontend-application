971ce544f988fef187e1dc5de71545cfa1193805
Christina <christina-f-94@web.de>
2018-08-11 20:21:31 +0200

    Make administration page TSLint conform
    Refers to issue #14

--- angular2/src/app/administration-page/administration-page.component.ts
+++ angular2/src/app/administration-page/administration-page.component.ts
@@ -1,20 +1,19 @@
 import {Component, OnInit} from '@angular/core';
-import {Router,} from '@angular/router';
-
+import {Router} from '@angular/router';
 import {AlertService} from '../services/alert.service';
 import {ApiService} from '../services/api.service';
-
 
 @Component({
     selector: 'administration-page',
     templateUrl: './administration-page.component.html',
     styleUrls: ['./administration-page.component.css']
 })
+
 export class AdministrationPageComponent implements OnInit {
-    title: string = 'Angular 2 with BPMN-JS';
-    model: any = {};
-    loading = false;
-    page: string = 'User';
+    public title: string = 'Angular 2 with BPMN-JS';
+    public model: any = {};
+    public loading: any = false;
+    public page: string = 'User';
 
     constructor(private router: Router,
                 private alertService: AlertService,
@@ -22,8 +21,8 @@ export class AdministrationPageComponent implements OnInit {
 
     }
 
-    ngOnInit() {
-
+    public ngOnInit(): void {
+        console.log('Administration page initialized');
     }
 
 }
