759d453c3fc85390abd6e16fbb8248a0c25e1a22
Unknown <s-mschm6@haw-landshut.de>
2018-08-13 21:23:14 +0200

    added test folder and example testfile with basic test examples

---
+++ angular2/src/test/classToTest.spec.ts
@@ -0,0 +1,10 @@
+describe('Meaningful Test', () => {
+    it('1 + 1 => 2', () => {
+        expect(1 + 1).toBe(2);
+    });
+
+    it('1 + 1 => !3', () => {
+        expect(1 + 1).not.toBe(3);
+    });
+
+});
