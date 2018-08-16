8558d944d662b168723463700d978001cf39ea92
Christina <christina-f-94@web.de>
2018-08-16 19:42:29 +0200

    Code style and documentation for front-page

--- angular2/src/app/front-page/front-page.component.ts
+++ angular2/src/app/front-page/front-page.component.ts
@@ -1,18 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-
 import {AlertService} from '../services/alert.service';
 import {ApiService} from '../services/api.service';
 
+//Include components for interface and styling
 @Component({
   selector: 'front-page',
   templateUrl: './front-page.component.html',
   styleUrls: ['./front-page.component.css']
 })
+
 export class FrontPageComponent implements OnInit {
-  title: string = 'Angular 2 with BPMN-JS';
-  model: any = {};
-  loading = false;
+  public title: string = 'Angular 2 with BPMN-JS';
+  public model: any = {};
+  public loading: boolean = false;
 
   constructor(private router: Router,
               private alertService: AlertService,
@@ -20,26 +21,28 @@ export class FrontPageComponent implements OnInit {
 
   }
 
-  ngOnInit() {
+  //Initialization after front-page component was loaded
+  public ngOnInit(): void {
     // reset login status
     this.apiService.logout()
       .subscribe(response => {
+        console.log('Successfully logged out!');
       }, error => {
         console.log(error);
-        this.alertService.error(error)
+        this.alertService.error(error);
       });
   }
 
-  login() {
+  // Login of user ans subscribe response of POST authenticate
+  public login() {
     this.loading = true;
     this.apiService.authenticate(this.model.username, this.model.password, this.model.captcha)
       .subscribe(response => {
           if (response.success) {
             console.log(JSON.stringify(response, null, 2));
             this.router.navigate(['/modeler']);
-          }
-          else {
-            this.alertService.error(response.error)
+          } else {
+            this.alertService.error(response.error);
           }
         },
         error => {
@@ -53,14 +56,13 @@ export class FrontPageComponent implements OnInit {
   debug1() {
     this.apiService.authenticate('1', '12341234', '1234')
       .subscribe(response => {
-          console.log(response)
+          console.log(response);
           this.router.navigate(['/modeler']);
         },
         error => {
           console.log('loginerror: ', error);
         });
   }
-
 
   debug2() {
     this.apiService.authenticate('2', '12341234', '1234')
@@ -71,5 +73,4 @@ export class FrontPageComponent implements OnInit {
           console.log(error);
         });
   }
-
 }
