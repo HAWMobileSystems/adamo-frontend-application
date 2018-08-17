4d32522834097445644742cfa20237748755e28c
Unknown <s-mschm6@haw-landshut.de>
2018-08-17 21:42:02 +0200

    Variablen Modal in HTML und TS gesplittet

--- angular2/src/app/ModelerComponent/modals/variables.component.ts
+++ angular2/src/app/ModelerComponent/modals/variables.component.ts
@@ -2,23 +2,7 @@ import {Component, Input} from '@angular/core';
 import {Variable} from './variable';
 @Component({
     selector: 'variable-comp',
-    template: `
-    <div>
-    <form>
-      <div class = "modal-form-group">
-        <label> Variable: </label>
-        <br>
-        <input type="text" [(ngModel)]="varName.name" class = "modal-form-input" [ngModelOptions]="{standalone: true}">
-        <br>
-        <label> Value: </label>
-        <br>
-        <input type="text" [(ngModel)]="varName.value" class = "modal-form-input" [ngModelOptions]="{standalone: true}">
-        <br>
-        <input type="checkbox" value="Meta?" [(ngModel)]="varName.meta" [ngModelOptions]="{standalone: true}"> Meta?
-      </div>
-    </form>
-  </div>
-	  `
+    templateUrl: './variables.component.html'
 })
 export class VariableComponent {
   @Input() public varName : Variable;
