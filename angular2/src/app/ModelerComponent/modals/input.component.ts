7c598f615538320b19086e85621665420cf33339
Unknown <s-mschm6@haw-landshut.de>
2018-08-17 21:41:28 +0200

    Input Component in HTML und TS gesplittet

--- angular2/src/app/ModelerComponent/modals/input.component.ts
+++ angular2/src/app/ModelerComponent/modals/input.component.ts
@@ -2,15 +2,7 @@ import {Component, Input} from '@angular/core';
 import {Variable} from './variable';
 @Component({
     selector: 'inputvar-comp',
-    template: `
-    <form>
-    <label> {{varName.name}}: </label>
-    <br>
-    <input type="text" [(ngModel)]="varName.value" class = "modal-form-input" [ngModelOptions]="{standalone: true}">
-    <br>
-  </form>
-
-	  `
+    templateUrl: './input.component.html'
 })
 export class InputVarComponent {
   @Input() public varName : Variable;
