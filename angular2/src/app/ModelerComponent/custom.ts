926f225ac8746be64f58ae0c9de0a98346b04cca
Unknown <s-mschm6@haw-landshut.de>
2018-08-17 17:35:22 +0200

    Auto stash before merge of "mark" and "origin/mark"

--- angular2/src/app/ModelerComponent/custom.ts
+++ angular2/src/app/ModelerComponent/custom.ts
@@ -2,5 +2,5 @@ export const Custom = {
     id: `CustomCompany`,
     name: `CustomCompany`,
     prefix: `ext`,
-    "uri": "http://custom.example.com/bpmn/2016",
+    uri: 'http://custom.example.com/bpmn/2016'
 };
