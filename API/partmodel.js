c4b46baaf63dc82dce8ef46e8f689380f53022a2
Unknown <s-mschm6@haw-landshut.de>
2018-08-29 02:41:23 +0200

    Reworked PartModel API for Version/Upsert

--- API/partmodel.js
+++ API/partmodel.js
@@ -48,11 +48,11 @@ router.get('/all', function (req, res) {
 * Description:      
 * */
 
-router.post('/partmodel', function (req, res) {
+router.post('/usage', function (req, res) {
 
-    const mid = req.body.mid;
+    const pmid = req.body.pmid;
 
-    db.query('select * from partialmodel where mid = $1', [mid])
+    db.query('select p.mid AS mid,p.version AS version, m.modelname AS modelname FROM partialmodel AS p JOIN model AS m ON p.mid=m.mid AND p.version=m.version WHERE p.pmid = $1 ORDER BY modelname ASC, version DESC', [pmid])
         .then(function (data) {
             if(data){
                 console.log('data is ', data);
@@ -84,28 +84,22 @@ router.post('/partmodel', function (req, res) {
     
 router.post('/create', function (req, res) {
     
-    if(!req.body.mid || !req.body.pid) {
-        res.status(400).send({ status: 'Models may not be empty!'});
+    if(!req.body.mid || !req.body.pmid || !req.body.version) {
+        res.status(400).send({ status: 'Models and Version may not be empty!'});
         return;
     }  
         
     const mid = req.body.mid;
-    const pid = req.body.pid;
+    const pmid = req.body.pmid;
+    const version = req.body.version;
         
-    db.oneOrNone('select * from partialmodel where mid = $1', [mid])
+    db.oneOrNone('insert into partialmodel (mid, version, pmid) values ($1,$2,$3)', [mid,version,pmid])
         .then(function (data) {
-            db.oneOrNone('insert into partialmodel (mid) values ($1)', [mid])
-                .then(function (data) {
-                    res.send({ status: 'Model created successfully as a partial model', success: true});
-                    })
-                .catch(function (error) {
-                    console.log('ERROR POSTGRES:', error)
-                    res.status(400).send({ status: 'Database not available'});
-                    })
-            })
+            res.send({ status: 'Model created successfully as a partial model', success: true});
+        })
         .catch(function (error) {
             console.log('ERROR POSTGRES:', error)
-            res.status(400).send({ status: 'Database not available'});
+            res.status(400).send({ status: 'Database not available', error: error});
         })
 });
 
@@ -125,22 +119,25 @@ router.post('/create', function (req, res) {
 
 router.post('/delete', function (req, res) {
 
-    if(!req.body.pmid) {
-        res.status(400).send({ status: 'Partial model may not be empty!'});
+    if(!req.body.mid) {
+        res.status(400).send({ status: 'Model may not be empty!'});
         return;
     }
-    const pmid = req.body.pmid;
+    if(!req.body.version) {
+        res.status(400).send({ status: 'Version may not be empty!'});
+        return;
+    }
+    const mid = req.body.mid;
+    const version = req.body.version;
 
-    console.log(pmid);
-
-    db.oneOrNone('select * from partialmodel where pmid = $1', [pmid])
+    db.oneOrNone('select * from partialmodel where mid = $1 and version = $2', [mid,version])
     .then(function (data) {
         if(data){
-            db.oneOrNone('delete from partialmodel where pmid = $1', [pmid])
+            db.oneOrNone('delete from partialmodel where mid = $1 and version = $2', [mid,version])
             console.log('data is ', data);
             res.send({ status: 'Partial model deleted successfully', success: true});
         } else {
-            res.status(400).send({ status: 'Partial model does not exist'})
+            res.send({ status: 'No Partmodels existed in this diagram version', success: true});
         }
     })
     .catch(function (error) {
