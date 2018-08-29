60a8c007b7f19271c943b8758f1e045613e0a7c3
Unknown <s-jespic@haw-landshut.de>
2018-08-29 15:09:36 +0200

    Commented permission.js and modification of error messages

--- API/permission.js
+++ API/permission.js
@@ -8,20 +8,20 @@ router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
 
-/*
-* URL:              /:user/:model
-* Method:           get
-* URL Params:
-*   Required:       none
-*   Optional:       none
-* Data Params:
-*   Required:       none
-*   Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:
-* */
-
+/**
+ * @api                 {get} /permission/:user/:model :model
+ * @apiDescription      Requests all permissions from the database.
+ * @apiName             all
+ * @apiGroup            permission
+ * @apiSuccess          {Array} data Array of permissions
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      [1, 2, 3, 4, 5, ...]
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ */
 router.get('/:user/:model', function (req, res) {
   const user = req.params.user;
   const model = req.params.model;
@@ -37,23 +37,29 @@ router.get('/:user/:model', function (req, res) {
     })
     .catch(function (error) {
       console.log('ERROR POSTGRES:', error);
-      res.status(400).send({status: 'Database not available', error: 'Database not available', success: false});
+      res.status(400).send({status: 'Something went wrong', success: false});
     })
 });
 
-/*
-* URL:              /create
-* Method:           get
-* URL Params:
-*   Required:       none
-*   Optional:       none
-* Data Params:
-*   Required:       none
-*   Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:
-* */
+
+/**
+ * @api                 {post} /permission/create create
+ * @apiDescription      Checks if post parameters modelid, userid and role are set,
+ *                      and creates a new permission.
+ * @apiName             create
+ * @apiGroup            permission
+ * @apiParam            {Int} modelid Mandatory modelid of a model
+ * @apiParam            {Int} userid Mandatory userid of a user
+ * @apiParam            {String} role Mandatory name of a role
+ * @apiSuccess          status Permission created successfully 
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      {status: 'Permission created successfully', success: true}
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ */
 router.post('/create', function (req, res) {
 
   if (!req.body.mid) {
@@ -77,28 +83,32 @@ router.post('/create', function (req, res) {
     'WHERE role = $3', [req.body.mid, req.body.uid, req.body.role])
     .then(function (data) {
       console.log('DATA:', data);
-      res.send({success: true, status: 'Permission successfully created'});
+      res.send({status: 'Permission created successfully', success: true});
     })
     .catch(function (error) {
       console.log('ERROR POSTGRES:', error);
-      res.status(400).send({status: 'Database not available', error: error, success: false});
+      res.status(400).send({status: 'Something went wrong', success: false});
     })
 });
 
-/*
-* URL:              /update
-* Method:           post
-* URL Params:
-*   Required:       none
-*   Optional:       none
-* Data Params:
-*   Required:       none
-*   Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:      
-* */
 
+/**
+ * @api                 {post} /permission/update update
+ * @apiDescription      Checks if post parameters permissionid and role are set,
+ *                      and updates the selected permission.
+ * @apiName             update
+ * @apiGroup            permission
+ * @apiParam            {Int} permissionid Mandatory permissionid of a permission
+ * @apiParam            {String} role Mandatory name of a role
+ * @apiSuccess          status Permission updated successfully
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      {status: 'Permission updated successfully', success: true}
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ */
 router.post('/update', function (req, res) {
 
   if (!req.body.pid) {
@@ -116,29 +126,33 @@ router.post('/update', function (req, res) {
     'WHERE pid = $1', [req.body.pid, req.body.role])
     .then(function (data) {
       console.log('DATA:', data);
-      res.send({success: true});
+      res.send({status: 'Permission updated successfully', success: true});
     })
     .catch(function (error) {
       console.log('ERROR POSTGRES:', error);
-      res.status(400).send({status: 'Database not available', error: error, success: false});
+      res.status(400).send({status: 'Something went wrong', success: false});
     })
 });
 
 
-/*
-* URL:              /delete
-* Method:           post
-* URL Params:
-*   Required:       none
-*   Optional:       none
-* Data Params:
-*   Required:       none
-*   Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:      
-* */
-
+/**
+ * @api                 {post} /permission/delete delete
+ * @apiDescription      Checks if post parameter permissionid is set,
+ *                      and deletes the selected permission from the database.
+ * @apiName             delete
+ * @apiGroup            permission
+ * @apiParam            {Int} permissionid Mandatory permissionid of a permission
+ * @apiSuccess          status Permission deleted successfully
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      {status: 'Permission deleted successfully', success: true}
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ *                      HTTP/1.1 404 Failure
+ *                      {status: 'Permission does not exist', success: false}
+ */
 router.post('/delete', function (req, res) {
 
   const pid = req.body.pid;
@@ -152,12 +166,12 @@ router.post('/delete', function (req, res) {
         console.log('data is ', data);
         res.send({status: 'Permission deleted successfully', success: true});
       } else {
-        res.status(400).send({status: 'Permission does not exist'})
+        res.status(404).send({status: 'Permission does not exist', success: false})
       }
     })
     .catch(function (error) {
       console.log('ERROR POSTGRES:', error)
-      res.status(400).send({status: 'Database not available'});
+      res.status(400).send({status: 'Something went wrong', success: false});
     })
 });
 
