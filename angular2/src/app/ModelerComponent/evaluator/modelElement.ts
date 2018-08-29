2510e94367ccb21c3c3147b13c01a084100b9c7f
Unknown <s-mschm6@haw-landshut.de>
2018-08-29 17:02:54 +0200

    added comments

--- angular2/src/app/ModelerComponent/evaluator/modelElement.ts
+++ angular2/src/app/ModelerComponent/evaluator/modelElement.ts
@@ -1,3 +1,4 @@
+//Simple class to define the basics structure for the cascading evaluation
 export class ModelElement {
     constructor(public name: string, public id: string, public xml: string) {
     }
