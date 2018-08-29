05ad96c30bf1469a3dda3310b826a02f2b853786
Unknown <s-jespic@haw-landshut.de>
2018-08-29 16:02:29 +0200

    Commented partmodel.js and modification of error messages

--- API/partmodel.js
+++ API/partmodel.js
@@ -6,48 +6,53 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
-/*
-* URL:              /all
-* Method:           get
-* URL Params:
-*   Required:       none
-*   Optional:       none
-* Data Params:
-*   Required:       none
-*   Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:      
-* */
 
+/**
+ * @api                 {get} /partmodel/all all
+ * @apiDescription      Requests all partial models from the database.
+ * @apiName             all
+ * @apiGroup            partmodel
+ * @apiSuccess          {Array} data Array of partial models
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      [1, 2, 3, 4, 5, ...]
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ */
 router.get('/all', function (req, res) {
 
     db.query('select * from partialmodel')
     .then(function (data) {
         console.log('DATA:', data)
-        res.send({ data: data, success: true});
+        res.send({data: data, success: true});
         })
         .catch(function (error) {
             console.log('ERROR POSTGRES:', error)
-            res.status(400).send({ status: 'Database not available'});
+            res.status(400).send({status: 'Something went wrong', success: false});
         })
 });
 
 
-/*
-* URL:              /partmodel
-* Method:           post
-* URL Params:
-*   Required:       none
-*   Optional:       none
-* Data Params:
-*   Required:       none
-*   Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:      
-* */
-
+/**
+ * @api                 {post} /partmodel/usage usage
+ * @apiDescription      Shows which models and which versions are maintained as a partial model. 
+ *                      The order of the models and the correspong version is 
+ *                      ascending reagrding the model name and descending reagrding the versions.
+ * @apiName             all
+ * @apiGroup            partmodel
+ * @apiSuccess          {Array} data Array of partial models
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      [1, 2, 3, 4, 5, ...]
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ *                      HTTP/1.1 404 Failure
+ *                      {status: 'Model does not exist', success: false}
+ */
 router.post('/usage', function (req, res) {
 
     const pmid = req.body.pmid;
@@ -56,36 +61,40 @@ router.post('/usage', function (req, res) {
         .then(function (data) {
             if(data){
                 console.log('data is ', data);
-                res.send({ data: data, success: true});
+                res.send({data: data, success: true});
             } else {
-                res.status(400).send({ status: 'Model does not exist'})
+                res.status(400).send({status: 'Model does not exist', success: false})
             }
         })
         .catch(function (error) {
             console.log('ERROR POSTGRES:', error)
-            res.status(400).send({ status: 'Database not available'});
+            res.status(400).send({status: 'Something went wrong', success: false});
         })
 });
 
 
-/*
-* URL:            /create
-* Method:         post
-* URL Params:
-* Required:       none
-* Optional:       none
-* Data Params:
-* Required:       none
-* Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:      
-* */
-    
+/**
+ * @api                 {post} /partmodel/create create
+ * @apiDescription      Checks if post parameters modelid, partmodelid and version are set,
+ *                      and creates a new partial model.
+ * @apiName             create
+ * @apiGroup            partmodel
+ * @apiParam            {Int} modelid Mandatory modelid of a model
+ * @apiParam            {Int} partmodelid Mandatory partmodelid of a partial model
+ * @apiParam            {Int} version Mandatory version of a model
+ * @apiSuccess          status Model created successfully as a partial model
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      {status: 'Model created successfully as a partial model', success: true}
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ */    
 router.post('/create', function (req, res) {
     
     if(!req.body.mid || !req.body.pmid || !req.body.version) {
-        res.status(400).send({ status: 'Models and Version may not be empty!'});
+        res.status(400).send({status: 'Models and Version may not be empty!'});
         return;
     }  
         
@@ -95,36 +104,42 @@ router.post('/create', function (req, res) {
         
     db.oneOrNone('insert into partialmodel (mid, version, pmid) values ($1,$2,$3)', [mid,version,pmid])
         .then(function (data) {
-            res.send({ status: 'Model created successfully as a partial model', success: true});
+            res.send({status: 'Model created successfully as a partial model', success: true});
         })
         .catch(function (error) {
             console.log('ERROR POSTGRES:', error)
-            res.status(400).send({ status: 'Database not available', error: error});
+            res.status(400).send({status: 'Something went wrong', success: false});
         })
 });
 
-/*
-* URL:              /delete
-* Method:           post
-* URL Params:
-*   Required:       none
-*   Optional:       none
-* Data Params:
-*   Required:       none
-*   Optional:       none
-* Success Response: Code 200, Content: {message: [string], success: [bool], data: [object]}
-* Error Response:   Code 400, Content: {message: [string], success: [bool]}
-* Description:      
-* */
 
+/**
+ * @api                 {post} /partmodel/delete delete
+ * @apiDescription      Checks if post parameters modelid and version are set,
+ *                      and deletes the selected partial model from the database.
+ * @apiName             delete
+ * @apiGroup            partmodel
+ * @apiParam            {Int} modelid Mandatory modelid of a model
+ * @apiParam            {Int} version Mandatory version of a model
+ * @apiSuccess          status Partial model  deleted successfully
+ * @apiSuccessExample   Success-Response:
+ *                      HTTP/1.1 200 OK
+ *                      {status: 'Partial model deleted successfully', success: true}
+ * @apiError            error Something went wrong
+ * @apiErrorExample     Error-Response:
+ *                      HTTP/1.1 400 Failure
+ *                      {status: 'Something went wrong', success: false}
+ *                      HTTP/1.1 404 Failure
+ *                      {status: 'No partial models exist in this diagram version', success: true}
+ */
 router.post('/delete', function (req, res) {
 
     if(!req.body.mid) {
-        res.status(400).send({ status: 'Model may not be empty!'});
+        res.status(400).send({status: 'Model may not be empty!'});
         return;
     }
     if(!req.body.version) {
-        res.status(400).send({ status: 'Version may not be empty!'});
+        res.status(400).send({status: 'Version may not be empty!'});
         return;
     }
     const mid = req.body.mid;
@@ -135,14 +150,14 @@ router.post('/delete', function (req, res) {
         if(data){
             db.oneOrNone('delete from partialmodel where mid = $1 and version = $2', [mid,version])
             console.log('data is ', data);
-            res.send({ status: 'Partial model deleted successfully', success: true});
+            res.status(200).send({status: 'Partial model deleted successfully', success: true});
         } else {
-            res.send({ status: 'No Partmodels existed in this diagram version', success: true});
+            res.status(404).send({status: 'No partial models exist in this diagram version', success: true});
         }
     })
     .catch(function (error) {
         console.log('ERROR POSTGRES:', error)
-        res.status(400).send({ status: 'Database not available'});
+        res.status(400).send({status: 'Something went wrong', success: false});
     })
 });
 
