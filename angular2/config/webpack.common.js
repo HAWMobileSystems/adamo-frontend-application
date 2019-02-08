2fb153c89e5c60bb7f1b3940bdd03b88025d1355
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@gmail.com>
2019-02-08 10:52:27 +0100

    poke the webpack.. create docker compose for production

--- angular2/config/webpack.common.js
+++ angular2/config/webpack.common.js
@@ -93,7 +93,7 @@ module.exports = {
         //     }
         // }),
         // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'bpmn',
+        //     name: 'bpmn',gitkr
         // }),
         // new webpack.optimize.CommonsChunkPlugin({
         //     name: 'vendor',
