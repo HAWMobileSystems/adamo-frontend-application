16fae373c920e903655d247d247f0ad993830445
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@gmail.com>
2019-02-25 08:21:05 +0100

    creating environments on the fly with some magic using yargs

---
+++ angular2/src/environments/environment.ts
@@ -0,0 +1,18 @@
+
+export const environment = {
+  production: false,
+  NODE_ENV: 'development',
+  SERVER_HOST: 'http://localhost',
+  SERVER_PORT: 3330,
+  MQTT_PORT: 4711,
+  DB_HOST: 'postgres',
+  DB_NAME: 'ipim',
+  DB_USER: 'postgres',
+  DB_PASSWORD: 'postgres',
+  DB_PORT: 5432,
+  DATA_SAVE_PATH: './postgres-data',
+  POSTGRES_IMAGE_VERSION: '11.1-alpine',
+  APP_HOST: 'http://localhost',
+  APP_PORT: 8085,
+  CAMUNDA_ENGINE_HOST: 'http://localhost:8080/engine-rest/deployment/create'
+};
