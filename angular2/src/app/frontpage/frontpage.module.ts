c0639b35a90e1f15bc3c4a119d98d4e355940983
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@gmial.com>
2019-07-25 09:37:26 +0200

    adding bulma design on frontpage

--- angular2/src/app/frontpage/frontpage.module.ts
+++ angular2/src/app/frontpage/frontpage.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FrontPageComponent} from './frontpage.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 // import { FrontPageComponent} from './FrontPageComponent.component';
+// import { HeaderComponent} from "../components/HeaderComponent/header.component"
 
 @NgModule({
   declarations: [ FrontPageComponent ],
