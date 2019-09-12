5af6986ae0cbf60c1a6ba19ab2249e28d1958fb6
Sebastian <s-sbilda@haw-landshut.de>
2019-09-12 20:51:19 +0200

    added http request for local json file

--- angular2/src/app/components/tutorial/services/level.service.ts
+++ angular2/src/app/components/tutorial/services/level.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from "@angular/core";
 import { Level, Helper } from '../models/Level';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
 
 @Injectable()
 export class LevelService {
 
+    data: string
+
+    constructor(private http: HttpClient){
+        this.getJSON().subscribe(data => this.data);
+    }
+
+    public getJSON(): Observable<any> {
+        return this.http.get("/assets/fixtures/test.json", { responseType: 'json'});
+    }
+    
 
     public getLevels() {
         let levels: Level[];
         let mcObjArrBasic = new Array;
-        mcObjArrBasic.push(this.createMCObjectArray("1-Frage Basic 1", ["1-Antwort Basic 1", "1-Antwort Basic 2", "1-Antwort Basic 3"]));
+        mcObjArrBasic.push(new Helper<string, string[]>("1-Frage Basic 1", ["1-Antwort Basic 1", "1-Antwort Basic 2", "1-Antwort Basic 3"]));
         mcObjArrBasic.push(this.createMCObjectArray("2-Frage Basic 1", ["2-Antwort Basic 1", "2-Antwort Basic 2", "2-Antwort Basic 3", "2-Antwort Basic 4"]));
 
         let mcobjarrAdvanced = new Array;
@@ -19,7 +31,7 @@ export class LevelService {
         mcobjarrProfessional.push(this.createMCObjectArray("1-Frage Professional 1", ["1-Antwort Professional 1", "1-Antwort Professional 2", "1-Antwort Professional 3"]));
         mcobjarrProfessional.push(this.createMCObjectArray("2-Frage Professional 1", ["2-Antwort Professional 1", "2-Antwort Professional 2", "2-Antwort Professional 3", "2-Antwort Professional 4"]));
 
-
+        console.log(JSON.parse(this.data));
         levels = [
             new Level(1, "Basic", [1, 2, 3, 4, 5],
                 ["page 1 with content", "page 2 mit content", "<p>page 3 mit content</p>", "page 4", "page 5 ende Intro"],
@@ -31,6 +43,9 @@ export class LevelService {
                 ["page 1 with content", "page 2 mit content", "page 3 mit content", "page 4", "page 5", "page 6", "page 7  ende Intro Pro"],
                 mcobjarrProfessional)
         ]
+    
+        
+        console.log(JSON.stringify(levels[0]));
 
         return levels;
     }
