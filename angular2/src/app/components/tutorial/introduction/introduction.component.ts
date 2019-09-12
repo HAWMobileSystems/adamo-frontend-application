0b08bf2776a5a7bbd49e3e2a1b77f313065d8ca5
Sebastian <s-sbilda@haw-landshut.de>
2019-09-12 19:33:22 +0200

    change introduction tutorial, code refactoring

--- angular2/src/app/components/tutorial/introduction/introduction.component.ts
+++ angular2/src/app/components/tutorial/introduction/introduction.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { Level } from '../models/Level';
+import { LevelService } from '../services/level.service';
 
 @Component({
   selector: 'app-introduction',
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.css']
 })
-export class IntroductionComponent implements OnInit {
 
-  constructor() { }
+
+export class IntroductionComponent implements OnInit {
+  pages: number;
+  currentpage: number;
+  
+  private sub: any;
+  levid: number;
+  level: Level;
+
+  constructor(private route: ActivatedRoute, private LevServ: LevelService) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => this.levid = params['id'])
+    this.level = this.LevServ.getLevel(this.levid);
+    
+    this.pages = this.level.PageDesc.length;
+    this.currentpage = 1;
+
+  }
+  
+  clickNext() {
+    if (this.currentpage < this.pages) {
+      this.currentpage += 1;
+    } else {
+      // button.text Introduction abschließen, go to test your knowlege
+    }
+  }
+
+  clickPrevious() {
+    if (this.currentpage > 1) {
+      this.currentpage -= 1;
+    }
   }
 
 }
