405ba28d6926414aa56abbcfb3f92f7ed2cd3f26
Daniel Hilpoltsteiner <daniel.hilpoltsteiner@gmial.com>
2020-01-14 22:24:32 +0100

    adding module for leaves - will be renamed to administration

---
+++ angular2/src/app/leaves/leaves.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from "@angular/router";
+
+@Component({
+  selector: 'app-leaves',
+  templateUrl: './leaves.component.html',
+  styleUrls: ['./leaves.component.css']
+})
+export class LeavesComponent{
+    private activeTab = '';
+    constructor(
+        private router: Router,
+      ) {
+        this.activeTab = router.url.split('/').slice(-1)[0]
+        this.router.events
+          .pipe(filter(event => event instanceof NavigationEnd))
+          .subscribe((event: NavigationEnd) => {
+            this.activeTab = event.url.split('/').slice(-1)[0]
+          }); 
+      }
+}
