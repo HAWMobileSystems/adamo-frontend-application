d2820898c0146dd3b6ddb12b3aeb0606d782d084
Sebastian <s-sbilda@haw-landshut.de>
2020-02-19 12:48:03 +0100

    change filepath for tutorial

--- angular2/src/app/overview/overview-routing.module.ts
+++ angular2/src/app/overview/overview-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
       {
         path: "tutorial",
         loadChildren: () =>
-          import(`./../components/tutorial/tutorial.module`).then(m => m.TutorialModule)
+          import(`./../components/TutorialComponent/tutorial.module`).then(m => m.TutorialModule)
       },
       // {
       //   path: "settings",
