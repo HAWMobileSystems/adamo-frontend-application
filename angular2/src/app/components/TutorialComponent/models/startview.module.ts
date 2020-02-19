747616624665a38684f7275636a5c22f81985b38
Sebastian <s-sbilda@haw-landshut.de>
2020-02-19 12:54:32 +0100

    added startview module

---
+++ angular2/src/app/components/TutorialComponent/models/startview.module.ts
@@ -0,0 +1,22 @@
+export class Startview {
+    constructor(name){
+        this.catName = name
+        this.tasks = new Array
+    }
+    catName: String
+    intro_status: any
+    mult_qs_res: any
+    tasks: ModellingTask[]
+}
+
+export class ModellingTask {
+
+    constructor(id, name, score){
+        this.id = id
+        this.name = name
+        this.score = score
+    }
+    id: String
+    name: String
+    score: Number
+}
