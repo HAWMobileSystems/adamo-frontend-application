3b6cc3d21e870e2450d55ff3f3e84661b0c57061
Sebastian <s-sbilda@haw-landshut.de>
2020-02-20 19:15:03 +0100

    complete startview of tutorial inklusive intro checked and mc

--- angular2/src/app/components/TutorialComponent/components/tutorialview/tutorialview.component.ts
+++ angular2/src/app/components/TutorialComponent/components/tutorialview/tutorialview.component.ts
@@ -11,82 +11,18 @@ import { User } from '../../../../models/user';
   styleUrls: ['./tutorialview.component.css']
 })
 export class TutorialViewComponent implements OnInit {
+  private userID: String
+  private startview: Startview[] = new Array()
 
   constructor(private catService: LevelService, private authService: AuthService) {
-
+    this.userID = this.authService.getCurrentUser().id
   }
-
-  user: User
-  startview: Startview[] = new Array()
-
-
+  
   ngOnInit() {
-    this.user = this.authService.getCurrentUser()
-
-    let json_response = [
-      {
-        "catName:Advanced": {
-          "intro_status": [],
-          "mult_qs_res": [],
-          "modellingtasks": [
-            {"id":"c612b6e3-b927-42e8-bf5f-39051c01ad9b",
-            "name":"Learn to create a Basic Advanced Diagramm(1/3)",
-            "score":0},
-            {"id":"8ba455f2-255b-4352-92e3-541b6500f94d",
-            "name":"Learn to create a Basic Advanced Diagramm(2/3)",
-            "score":0},
-            {"id":"2842ee93-3638-441f-a142-251a6975a9f4",
-            "name":"Learn to create a Basic Advanced Diagramm(3/3)",
-            "score":0}
-            ]
-        }
-      }
-    ]
-
-    let json =[
-      {
-        "catName":"Beginner",
-        "id":"id-1-beginner-test",
-        "name":"name-1-beginner-test",
-        "score":56,
-        "intro":"true",
-        "mctest":"true"
-      },
-      {
-        "catName":"Beginner",
-        "id":"id-2-beginner-test",
-        "name":"name-2-beginner-test",
-        "score":0,
-        "intro":"true",
-        "mctest":"true"
-      },
-      {
-        "catName":"Advanced",
-        "id":"id-1-advanced-test",
-        "name":"name-1-advanced-test",
-        "score":0,
-        "intro":"false",
-        "mctest":"false"
-      },
-      {
-        "catName":"Profi",
-        "id":"id-1-profi-test",
-        "name":"name-1-profi-test",
-        "score":0,
-        "intro":"false",
-        "mctest":"false"
-      },
-
-    ]
-    this.parseIncomingJSON(json)
-    console.log(this.user.getUid)
-    
-    console.log(this.startview)
-
-    this.catService.startview(this.user.getUid).subscribe((view:any) => {
-      console.log(view)
+    this.catService.startview(this.userID).subscribe((view:any) => {
       this.parseIncomingJSON(view)
     })
+    console.log(this.startview)
   }
 
   parseIncomingJSON(json){
