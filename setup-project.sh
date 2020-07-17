69900da79c9d3b7105a9a74e51d101164041c058
Daniel Hilpoltsteiner <daniel.hilpoltsteinre@gmail.com>
2020-07-17 11:25:20 +0200

    add adamo.nginx.conf and update setup script

--- setup-project.sh
+++ setup-project.sh
@@ -1,7 +1,25 @@
-#! /bin/sh
-# mv .env.example .env
+#! /bin/
+echo Installiere PM2 Global
+npm install -g pm2
 
+cp .env.example .env
+cp .env.example .env.dev
+
+echo Installiere Frontend
 cd angular2
 npm install
+
+echo Baue Frontend
 npm run build
+
 cd ..
+
+echo Installiere Backend
+cd API
+npm install 
+ 
+echo Kopiere Artefakte an die richtigen Orte
+cd ..
+mv adamo.nginx.conf /etc/nginx/sites-enabled/adamo
+cp -r angular/dist/* /var/www/adamo 
+service nginx restart
