afe96b176805a4ad25861f9b50c0be5ebc8d400a
Daniel Hilpoltsteiner <daniel.hilpoltsteinre@gmail.com>
2020-08-04 16:29:42 +0200

    (security) fix token validation for user login

--- src/app/interceptor/token.interceptor.ts
+++ src/app/interceptor/token.interceptor.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { AuthService } from "../services/auth.service";
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
+import { UserWithTokenDto } from '../models/dto/UserWithTokenDTO';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-
+  private currentUserToken: any = null;
   constructor(public auth: AuthService) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
-    request = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${this.auth.getToken()}`
-      }
-    });
+  public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    try {
+      this.currentUserToken = this.auth.getCurrentUserWithTokenDto().token;
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.currentUserToken}`,
+        },
+      });
+    } catch (error) {
+      console.log('No User at the Moment');
+    }
+
 
     return next.handle(request);
   }
