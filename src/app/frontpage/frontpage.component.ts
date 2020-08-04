cbbf8720917925d3463c51584224dbe024490e20
Daniel Hilpoltsteiner <daniel.hilpoltsteinre@gmail.com>
2020-08-04 16:32:57 +0200

    (lint) lint fixes

--- src/app/frontpage/frontpage.component.ts
+++ src/app/frontpage/frontpage.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
-import { AlertService } from "../services/alert.service";
-import { ApiService } from "../services/api.service";
-import { AdamoMqttService } from "../services/mqtt.service";
-import { environment } from "../../environments/environment";
-import { NGXLogger } from "ngx-logger";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlertService } from '../services/alert.service';
+import { ApiService } from '../services/api.service';
+import { AdamoMqttService } from '../services/mqtt.service';
+import { environment } from '../../environments/environment';
+import { NGXLogger } from 'ngx-logger';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 // import { AuthService } from "../services";
-import { first } from "rxjs/operators";
-import { AuthService } from "../services";
+import { first } from 'rxjs/operators';
+import { AuthService } from '../services';
+import { IUserLoginDto, IUserLoginDtoImpl } from '../../client/interface/components/i-user-login-dto';
+import { UserDto } from '../models/dto/UserDTO';
 
 // import {TranslateService} from '@ngx-translate/core';// import {version} from "./../../../package.json"; TODO
 //Include components for interface and styling
 @Component({
-  selector: "front-page",
-  templateUrl: "./frontpage.component.html",
-  styleUrls: ["./frontpage.component.less"]
+  selector: 'front-page',
+  templateUrl: './frontpage.component.html',
+  styleUrls: ['./frontpage.component.less'],
 })
 export class FrontPageComponent implements OnInit {
   public loginForm: FormGroup;
 
-  private project = environment.PROJECTNAME;
+  private currentUser: UserDto = null;
+  private project: string = environment.PROJECTNAME;
   public loading = false;
   public submitted = false;
   private returnUrl: string;
-  public error = "";
+  public error = '';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -34,20 +37,24 @@ export class FrontPageComponent implements OnInit {
     private authService: AuthService,
   ) {
     // redirect to home if already logged in
-    // if (this.authService.currentUserValue) {
-    //   this.router.navigate(["/"]);
-    // }
-  
+    try {
+      this.currentUser = this.authService.getCurrentUser();
+      if (this.currentUser) {
+        this.router.navigate(['/']);
+      }
+    } catch (error) {
+      console.log('No User found');
+    }
   }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required],
     });
 
     // get return url from route parameters or default to '/'
-    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
   }
 
   // convenience getter for easy access to form fields
@@ -55,20 +62,21 @@ export class FrontPageComponent implements OnInit {
     return this.loginForm.controls;
   }
 
-  onSubmit() {
-    this.logger.debug("submit pressed");
+  public onSubmit() {
+    this.logger.debug('submit pressed');
     this.submitted = true;
 
     // stop here if form is invalid
     if (this.loginForm.invalid) {
-      this.logger.debug("login form invalid");
+      this.logger.debug('login form invalid');
       return;
     }
 
-    this.logger.debug("form valid");
+    this.logger.debug('form valid');
     this.loading = true;
+    const userLoginDto: IUserLoginDto = { email: this.f.email.value, password: this.f.password.value };
     this.authService
-      .login(this.f.email.value, this.f.password.value)
+      .login(userLoginDto)
       .pipe(first())
       .subscribe(
         (data: any) => {
@@ -77,13 +85,13 @@ export class FrontPageComponent implements OnInit {
           // this.router.navigate([this.returnUrl]);
         },
         (error: any) => {
-          this.logger.debug("error received ", error);
+          this.logger.debug('error received ', error);
           this.error = error.statusText;
-          if(error.name === "HttpErrorResponse") {
-            this.error = "Server is not reachable";
+          if (error.name === 'HttpErrorResponse') {
+            this.error = 'Server is not reachable';
           }
           this.loading = false;
-        }
+        },
       );
   }
 }
